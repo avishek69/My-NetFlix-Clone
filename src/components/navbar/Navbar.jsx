bab57c9ef87b72a11197b204616dfa19d84d32ae
Avishek <avishekd653@gmail.com>
2022-04-18 12:08:47 +0530

    done FE

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png "
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://cdn.pixabay.com/photo/2020/08/10/23/33/character-5478928_960_720.png"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
